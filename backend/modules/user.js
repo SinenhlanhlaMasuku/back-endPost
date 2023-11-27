189d5d8c5306e7006b1b06093f82ba94bc4ddf2f
SinenhlanhlaMasuku <118171418+SinenhlanhlaMasuku@users.noreply.github.com>
2023-11-27 09:54:39 +0200

    updates

---
+++ backend/modules/user.js
@@ -0,0 +1,13 @@
+const db =require('../util/database');
+
+module.exports = class User{
+    constructor(name, email, password) {
+
+    this.name = name;
+    this.email = email;
+    this.password = this.password;
+
+  }
+};
+
+
